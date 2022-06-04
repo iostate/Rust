683e806128891be40cdca6e4770e0e63bae882b7
iostate <qmtruong92@gmail.com>
2022-06-04 12:40:48 -0700

    Exercise B Functions - Calculate the area and volume

--- B_Functions/src/lib.rs
+++ B_Functions/src/lib.rs
@@ -1,3 +1,10 @@
+// Procedural macros need importing directly
+// use rust_decimal_macros::dec;
+
+// If the reexportable feature is enabled, `Decimal` needs to be in scope
+// #[cfg(feature = "reexportable")]
+// use rust_decimal::Decimal;
+
 /**
  * First calculate the area of the thing.
  */
@@ -13,3 +20,15 @@ pub fn area_of(x: i32, y: i32) -> i32 {
     //            Hint: `cargo clippy` will warn you about this exact thing.
     x * y
 }
+
+// pub fn data_types() {
+//     // let mut number = dec!(1.2345);
+//     // let mut result = assert_eq!("1.2345", number.to_string());
+//     // println!("Number: {}", number);
+//     // number = dec!(-5.4321);
+//     // result = assert_eq!("-5.4321", number.to_string());
+
+//     // let number = -1.23;
+//     let decimal = dec!(-1.23);
+//     let equals = assert_eq!("-1.23", decimal.to_string());
+// }
