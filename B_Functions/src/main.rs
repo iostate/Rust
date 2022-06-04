da04026e78313838cb5377e86b703b06c76451ac
iostate <qmtruong92@gmail.com>
2022-06-04 12:39:48 -0700

    Exercise B - Functions

--- B_Functions/src/main.rs
+++ B_Functions/src/main.rs
@@ -1,25 +1,46 @@
 // Silence some warnings so they don't distract from the exercise.
-#![allow(unused_variables)]
+// #![allow(unused_variables)]
+
+use b_functions::area_of;
+// use b_functions::data_types;
 
 fn main() {
-    let width = 4;
+    // let x = (let y = 6);
+    // Produces an error
 
-    let height = 7;
-    let depth = 10;
+    // let width = 4;
+    // let height = 7;
+    // let depth = 10;
+    let (width, height, depth) = (4, 7, 10);
+    let room_volume = volume(width, height, depth);
+    println!("The room's volume is {}", room_volume);
     // 1. Try running this code with `cargo run` and take a look at the error.
     //
     // See if you can fix the error. It is right around here, somewhere.  If you succeed, then
     // doing `cargo run` should succeed and print something out.
+    // Bonus. Perform variable shadowing
+    // Variable shadowing with one type BEGIN
+    let size_of_cube = volume(width, height, depth);
     {
-        let area = b_functions::area_of(width, height);
+        let area = area_of(width, height);
+
         println!("Area is {}", area);
 
         let size_of_cube = volume(width, height, depth);
-        println!("The volume is calculated by multiplying the width, {}, with the height, {}, then multiplying the result of that with the depth, {}, to get the result of {}", width, height, depth, size_of_cube);
+        println!("The area is {}", size_of_cube);
+        println!("Variables within this block, area, size_of_cube, immediately go to Garbage collection after the block ends. ");
     }
-
-    // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
-    //    the code again and make sure it worked (you should get an area of 28).
+    println!("The area is {}", size_of_cube); // Variable shadowing with one type END
+                                              // let y = {
+                                              //     let x = 3;
+                                              //     x + 1
+                                              // };
+                                              // println!("The value of y is: {}", y);
+                                              // Prints 4
+                                              // println!("The value of x is: {}", x);
+                                              // Produces an error
+                                              // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
+                                              //    the code again and make sure it worked (you should get an area of 28).
 
     // 3. Uncomment the line below.  It doesn't work yet because the `volume` function doesn't exist.
     //    Create the `volume` function!  It should:
@@ -29,9 +50,21 @@ fn main() {
     //
     // If you get stuck, remember that this is *very* similar to what `area_of` does.
     //
-    //println!("Volume is {}", volume(width, height, depth));
+    println!("Volume is {}", volume(width, height, depth));
 }
 
+///     Calculates the volume based on data provided by the user.
+///
+///     # Examples
+///
+/// ```
+///     let (width, height, depth) = (4, 7, 10);
+///     let roomVolume = volume(width, height, depth);
+/// ```
+///     @width  i32 Width to be added to the volume calculation
+///     @height i32 Height to be added to the volume calculation
+///     @depth  i32 Depth to be added to the volume calculation
+///
 fn volume(width: i32, height: i32, depth: i32) -> i32 {
     width * height * depth
 }
